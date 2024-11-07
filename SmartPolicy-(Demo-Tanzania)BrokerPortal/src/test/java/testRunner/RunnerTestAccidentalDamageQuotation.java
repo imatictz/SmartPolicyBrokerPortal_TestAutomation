876aede70707f9d41e4d812bbc01376548163ccf
PravinSWagh5 <167967945+PravinSWagh5@users.noreply.github.com>
2024-11-07 18:54:21 +0530

    Update RunnerTestAccidentalDamageQuotation.java

--- SmartPolicy-(Demo-Tanzania)BrokerPortal/src/test/java/testRunner/RunnerTestAccidentalDamageQuotation.java
+++ SmartPolicy-(Demo-Tanzania)BrokerPortal/src/test/java/testRunner/RunnerTestAccidentalDamageQuotation.java
@@ -4,9 +4,9 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(  
-		           features="src/test/resources/Quotations/AccidentalDamage.feature", 
+		           features="src/test/resources/Quotations/Bonds.feature", 
                    tags= "@MandatoryFields", 
-                   glue={"cucumberMap3AccidentalDamage","MyHooks"}, 
+                   glue={"cucumberMap5Bonds","MyHooks"}, 
                    monochrome=true,   
                    plugin= { "pretty",   	
                            "html:target/CucumberTest/CucumbetReport.html"},
